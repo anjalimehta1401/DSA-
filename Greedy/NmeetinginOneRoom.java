0ff4ea13bedd67275b8b55c9085f2de1cceb5587
anjalimehta1401 <47583941+anjalimehta1401@users.noreply.github.com>
2025-08-20 09:02:36 +0530

    Update NmeetinginOneRoom.java

--- Greedy/NmeetinginOneRoom.java
+++ Greedy/NmeetinginOneRoom.java
@@ -1,57 +1,49 @@
 import java.util.*;
 
-class meeting {
-    int start;
-    int end;
-    int pos;
-
-    meeting(int start, int end, int pos) {
+class Meeting {
+    int start, end, pos;
+    Meeting(int start, int end, int pos) {
         this.start = start;
         this.end = end;
         this.pos = pos;
     }
 }
 
-public class Meeting {
-    static void maxMeetings(int start[], int end[], int n) {
-        ArrayList<meeting> meet = new ArrayList<>();
+public class MaxMeetings {
 
-        for (int i = 0; i < n; i++)
-            meet.add(new meeting(start[i], end[i], i + 1));
+    static void maxMeetings(int[] start, int[] end) {
+        int n = start.length;   
 
-        // simpler comparator
-        Collections.sort(meet, 
-            Comparator.comparingInt((meeting m) -> m.end)
+        List<Meeting> meet = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            meet.add(new Meeting(start[i], end[i], i + 1));
+        }
+
+        // sort by end time, then by position
+        Collections.sort(meet,
+            Comparator.comparingInt((Meeting m) -> m.end)
                       .thenComparingInt(m -> m.pos));
 
-        ArrayList<Integer> answer = new ArrayList<>();
+        List<Integer> answer = new ArrayList<>();
         answer.add(meet.get(0).pos);
         int limit = meet.get(0).end;
 
         for (int i = 1; i < n; i++) {
             if (meet.get(i).start > limit) {
-                answer.add(meet.get(i).pos);
                 limit = meet.get(i).end;
+                answer.add(meet.get(i).pos);
             }
         }
 
-        System.out.println("The order in which the meetings will be performed is ");
+        System.out.println("The order in which the meetings will be performed is:");
         for (int pos : answer) {
             System.out.print(pos + " ");
         }
     }
 
-    public static void main(String args[]) {
-        int n = 6;
-        int start[] = {1, 3, 0, 5, 8, 5};
-        int end[] = {2, 4, 5, 7, 9, 9};
-        maxMeetings(start, end, n);
+    public static void main(String[] args) {
+        int[] start = {1, 3, 0, 5, 8, 5};
+        int[] end   = {2, 4, 5, 7, 9, 9};
+        maxMeetings(start, end);
     }
 }
-
-===============================
-
-
-  Time Complexity: O(n log n) (because of sorting)
-
-Space Complexity: O(n) (for storing meetings & answer list)
